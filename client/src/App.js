bbb1ce8e3a723e05826fb43daa79e2b5045748cf
Emil <emilskovmand@gmail.com>
2022-06-09 14:41:31 +0200

    react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Login from "./containers/login";
+import { AuthProvider } from "./hooks/useProvideAuth";
+import Admin from "./containers/administration";
+import Index from "./containers";
 
 function App() {
     return (
         <>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="admin" element={<Login />} />
-            </Routes>
+            <AuthProvider>
+                <Router>
+                    <Routes>
+                        <Route exact path="/" element={<Index />} />
+                        <Route exact path="/login" element={<Login />} />
+                        <Route exact path="/admin" element={<Admin />} />
+                    </Routes>
+                </Router>
+            </AuthProvider>
         </>
     );
 }
--- client/src/containers/administration.js
+++ client/src/containers/administration.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Administration() {
-    return <div>administration</div>;
+    return (
+        <>
+            <div>administration</div>
+        </>
+    );
 }
