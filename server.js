36989dfcc6e1c40b54771c748c9e2fbe872dd9af
Emil <emilskovmand@gmail.com>
2022-06-09 10:09:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.use(function (err, req, res, next) {
     res.locals.error = req.app.get("env") === "development" ? err : {};
 
     // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+    res.status(500);
+    res.json({ message: "Something went wrong." });
 });
 
 // Lyt til port 3001 ELLLER den dynamiske port fra hosten
