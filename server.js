e230c679d6eedf1353fd7c74d9a17e2631e95ec6
Emil <emilskovmand@gmail.com>
2022-06-09 09:22:29 +0200

    dotenv

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var app = express();
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {});
+app.set("json spaces", 2); // sets JSON spaces for clarity
+require("dotenv/config");
+app.use(cookieParser(process.env.SECRET));
 
 // error handler
 app.use(function (err, req, res, next) {
